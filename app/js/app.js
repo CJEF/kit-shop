7619b355bfae2e544b182b64f0bec5a6e28f7d88
CJEF <ef2200@mail.ru>
2021-04-19 17:57:05 +0600

    scss not full

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,10 @@
-$(document).ready(function() {
-	// $('body').hide()
-})
+const swiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 1,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
