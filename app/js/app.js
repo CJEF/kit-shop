f0265e73716e117f0915858e7ff065874908240e
CJEF <ef2200@mail.ru>
2021-04-20 17:56:30 +0600

    first

--- app/js/app.js
+++ app/js/app.js
@@ -8,3 +8,23 @@ const swiper = new Swiper(".swiper-container", {
     el: ".swiper-pagination",
   },
 });
+
+$(document).on("click", "#video-btn", function (e) {
+  //отменяем стандартное действие button
+  e.preventDefault();
+  var poster = $(this);
+  // ищем родителя ближайшего по классу
+  var wrapper = poster.closest("#video-wrap");
+  videoPlay(wrapper);
+});
+
+//вопроизводим видео, при этом скрывая постер
+function videoPlay(wrapper) {
+  var iframe = wrapper.find("#video-btn");
+  // Берем ссылку видео из data
+  var src = iframe.data("src");
+  // скрываем постер
+  wrapper.addClass("videoWrapperActive");
+  // подставляем в src параметр из data
+  iframe.attr("src", src);
+}
